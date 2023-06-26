4e5083d4a36ade0af586a9f3d8480dcbdfd13f80
werzet <10843461+Werzet@users.noreply.github.com>
2023-06-26 21:12:47 +1000

    Добавил библиотеку yaml

---
+++ SS14.Watchdog/Controllers/InstanceConfigurationController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SS14.Watchdog.Controllers
+{
+	[Route("/instanceConfiguration/{key}")]
+	[Controller]
+	public class InstanceConfigurationController : ControllerBase
+	{
+		[HttpPost("{path}/{value}")]
+		public void Create(string path, string value)
+		{
+
+		}
+
+		[HttpPut("{path}/{value}")]
+		public void Update(string path, string value)
+		{
+
+		}
+
+		[HttpDelete("{path}")]
+		public void Delete(string path, string value)
+		{
+
+		}
+
+		[HttpGet("{path}")]
+		public void Get(string path, string value)
+		{
+
+		}
+	}
+}
